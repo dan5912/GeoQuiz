d44382b628fba67f90f469c2887cff8899c2e87a
dan5912 <danh5912@gmail.com>
2016-01-31 11:48:24 -0600

    pg. 185
    Before first challenge

---
+++ app/src/main/java/info/jdelectronics/android/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package info.jdelectronics.android.geoquiz;
+
+/**
+ * Created by daniel on 1/29/16.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+}
